8a7a77301639a1bb1e3595a7ec250f1a7c68f0c7
Tom Wilhoit <tomwilhoit.business@gmail.com>
2019-04-13 18:10:39 -0600

    Matches Backend and API data and displays on page

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,16 +1,26 @@
 import React from 'react'
+import './Card.scss'
 import { connect } from "react-redux";
 
-export const Card = (place) => {
-  console.log(this.props.weather)
+export const Card = (props) => {
+  if(props.weather.length){
+    const temperature = props.weather[props.place.id].currently.apparentTemperature
+    const summary = props.weather[props.place.id].daily.summary
+    console.log(props.weather[props.place.id])
+    console.log(props.index)
+    console.log(props.place.id)
+  
   return (
-    <div className='PlaceCard'>
-      <h3>{place.place.ficName}</h3>
-      <h4>{place.place.lat}</h4>
-      <h4>{place.place.long}</h4>
-      <h4>{place.place.id}</h4>
+    <div className='place-card'>
+      <h2>{props.place.ficName}</h2>
+      <h1>{temperature} F</h1>
+      <h4>{summary}</h4>
+      <h5>Inspiration: {props.place.realName}</h5>
     </div>
   )
+  }else{
+    return(<div>Loading</div>)
+  }
 }
 
 export const mapStateToProps = state => ({
